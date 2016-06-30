61feef30a173f1322b4be2c736d366d0eba43fe4
chendaole <chendaole@4399inc.com>
2016-06-30 13:34:24 +0800

    fix: 修改提示语

--- routes/resources.js
+++ routes/resources.js
@@ -16,24 +16,23 @@ router.get('/test', function(req, res, next) {
 
 router.get('/home-slidedata',function(req, res ,next){
   console.log("请求获取主页相册图片");
-  console.log('请求的URL路径:', req.originalUrl);
   res.contentType('json');
   res.send(slideData);
   res.end();
 });
 
 router.get('/home-cardsdata-test',function(req, res, next){
-  console.log("请求获取主页卡片信息");
-  console.log('请求的URL路径:', req.originalUrl);
+  console.log("本地Json");
   res.contentType('json');
   res.send(cardsData);
   res.end();
 });
 
 router.get('/couchDB-home-cardsdata',function(req ,res,next){
+  console.log("ready read DB")
   db.get('_design/cardsdata/_view/cardsdata', function (error, body, headers) {
     if(error) { return res.send(error.message, error['status-code']); }
-    console.log("成功从数据库中获取数据");
+    console.log("success ! GET DB CardsData");
     res.send(body, 200);
   });
 });
