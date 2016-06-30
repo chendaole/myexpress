94066f88c6001ed11edaf37b2874b57db727f6da
chendaole <chendaole@4399inc.com>
2016-06-30 12:59:22 +0800

    fix：链接数据库

--- routes/resources.js
+++ routes/resources.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var db = require('nano')('http://localhost:5984/testdb');
+var db = require('nano')('http://localhost:5984/myexpress');
 
 var router = express.Router();
 
@@ -22,7 +22,7 @@ router.get('/home-slidedata',function(req, res ,next){
   res.end();
 });
 
-router.get('/home-cardsdata',function(req, res, next){
+router.get('/home-cardsdata-test',function(req, res, next){
   console.log("请求获取主页卡片信息");
   console.log('请求的URL路径:', req.originalUrl);
   res.contentType('json');
@@ -30,10 +30,10 @@ router.get('/home-cardsdata',function(req, res, next){
   res.end();
 });
 
-router.get('/couchDB',function(req ,res,next){
-  db.get('62c71053fc481d89ab90cda75100035e', function (error, body, headers) {
+router.get('/couchDB-home-cardsdata',function(req ,res,next){
+  db.get('_design/cardsdata/_view/cardsdata', function (error, body, headers) {
     if(error) { return res.send(error.message, error['status-code']); }
-    console.log(body);
+    console.log("成功从数据库中获取数据");
     res.send(body, 200);
   });
 });
